3b2227556f004ce2b6bc0c60872d87d7d3fe22bf
Funmilayo Aina <84969489+Funmilayo-Aina@users.noreply.github.com>
2022-01-11 17:42:20 +0200

    Data Types--day two added to repo

---
+++ main.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+print(type(two_digit_number))
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+Very_new = print(first_number + second_number)
